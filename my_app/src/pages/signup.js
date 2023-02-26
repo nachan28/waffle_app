93bfccf253c2842d2f84574b30474d510e9e2bf5
nachan28 <torajiro282828@gmail.com>
2023-02-26 19:46:04 +0900

    追加: アカウント新規作成機能を追加

--- my_app/src/pages/signup.js
+++ my_app/src/pages/signup.js
@@ -1,9 +1,41 @@
-function signup() {
-    return (
-        <div>
-            アカウント新規作成
-        </div>
+import { useAuthContext } from "components/context/state";
+import { auth } from "components/firebase/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/router";
+
+function Signup() {
+  const router = useRouter();
+  const { setIsAuth, email, setEmail, password, setPassword } = useAuthContext();
+  const postUserInfo = () => {
+    createUserWithEmailAndPassword(auth, email, password).then(
+      (userCredential) => {
+        setIsAuth(true);
+        localStorage.setItem("isAuth", true);
+        router.push("/profileTA");
+      }
     );
+  };
+
+  return (
+    <>
+      <h1>アカウント新規作成</h1>
+      <input
+        type="text"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        placeholder="メールアドレス"
+      />
+      <input
+        type="text"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        placeholder="パスワード"
+      />
+      <button onClick={postUserInfo}>作成</button>
+    </>
+  );
 }
 
-export default signup;
+export default Signup;
