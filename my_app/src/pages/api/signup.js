10c8dc634aefe3f46488e3807b315567d4f98abe
nachan28 <torajiro282828@gmail.com>
2023-02-26 19:51:15 +0900

    追加: DB周り頑張ってる

---
+++ my_app/src/pages/api/signup.js
@@ -0,0 +1,16 @@
+// import { PrismaClient } from "@prisma/client"
+
+// const prisma = new PrismaClient();
+// export default async function handler(req, res) {
+//   const {username, email} = req.body;
+//   const user = await prisma.user.create({
+//     data: {
+//       name: username,
+//       email: email,
+//     }
+//   });
+//   return res.json(user);
+// }
+
+
+
