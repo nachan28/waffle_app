6a141dd12df090cd844d9b56c9ae8a2b55597b3c
Aimy <aiminagasawa@gmail.com>
2023-03-22 20:07:57 +0900

    フォーム作成

--- my_app/src/component/select.js
+++ my_app/src/component/select.js
@@ -1,144 +1,129 @@
 import { useState } from "react";
+import styles from "components/styles/profileTA.module.css";
 
 const Language = () => {
-  const [animals, setAnimals] = useState([
-    { label: "HTML",  checked: false },
-    { label: "CSS",  checked: false },
-    { label: "Javascript",  checked: false },
-    { label: "Python",  checked: false }
+  const [skills, setSkills] = useState([
+    { label: "HTML",  checked: false , id :1},
+    { label: "CSS",  checked: false ,id :2},
+    { label: "Javascript",  checked: false ,id :3},
+    { label: "Python",  checked: false,id:4 },
+    { label: "Java",  checked: false , id :5},
+    { label: "C",  checked: false ,id :6},
+    { label: "C#",  checked: false ,id :7},
+    { label: "PHP",  checked: false,id:8 },
+    { label: "Ruby",  checked: false,id:9},
+    { label: "TypeScript",  checked: false,id:10},
   ]);
 
   const [fields, setFields] = useState([
-    { label: "HTML",  checked: false },
-    { label: "CSS",  checked: false },
-    { label: "Javascript",  checked: false },
-    { label: "Python",  checked: false }
+    { label: "HealthTech",  checked: false , id :1},
+    { label: "MedTech",  checked: false ,id :2},
+    { label: "FinTech",  checked: false ,id :3},
+    { label: "Game",  checked: false,id:4 },
+    { label: "Google",  checked: false,id:4 }
+
+
   ]);
 
   const [sum, setSum] = useState(0);
   const onChange = (e) => {
-    const newAnimals = animals.map((animal) => {
-      const newAnimal = { ...animal };
+    const newLanguage = skills.map((skills) => {
+      const newAnimal = { ...skills };
       if (newAnimal.label === e.target.value) {
         newAnimal.checked = !newAnimal.checked;
       }
       return newAnimal;
     });
-    setAnimals(newAnimals);
+    setSkills(newLanguage);
     
-    newAnimals
-      .filter((animal) => animal.checked);
+    newLanguage
+      .filter((skills) => skills.checked);
     
   };
   return (
     <div>
        
-          <div key={animals.label}>
+          <div key={skills.label} className = {styles.formField}>
           <span>アドバイス出来る言語</span>
             <input
-              id={animals[0].label}
+              id={skills[0].label}
               type="checkbox"
-              value={animals[0].label}
+              value={skills[0].label}
               onChange={onChange}
             />
-            <label htmlFor={animals[0].label}>
-              {animals[0].label}
+            <label htmlFor={skills[0].label}>
+              {skills[0].label}
             </label>
 
             <input
-              id={animals[1].label}
+              id={skills[1].label}
               type="checkbox"
-              value={animals[1].label}
+              value={skills[1].label}
               onChange={onChange}
             />
-            <label htmlFor={animals[1].label}>
-              {animals[1].label}
+            <label htmlFor={skills[1].label}>
+              {skills[1].label}
             </label>
 
             <input
-              id={animals[2].label}
+              id={skills[2].label}
               type="checkbox"
-              value={animals[2].label}
+              value={skills[2].label}
               onChange={onChange}
             />
-            <label htmlFor={animals[2].label}>
-              {animals[2].label}
+            <label htmlFor={skills[2].label}>
+              {skills[2].label}
             </label>
             
             <input
-              id={animals[3].label}
+              id={skills[3].label}
               type="checkbox"
-              value={animals[3].label}
+              value={skills[3].label}
               onChange={onChange}
             />
-            <label htmlFor={animals[3].label}>
-              {animals[3].label}
+            <label htmlFor={skills[3].label}>
+              {skills[3].label}
             </label>
 
             
             </div>
 
-            <div key={animals.label}>
+            <div key={skills.label}>
           <span>働いている分野</span>
             <input
-              id={animals[0].label}
+              id={fields[0].label}
               type="checkbox"
-              value={animals[0].label}
+              value={fields[0].label}
               onChange={onChange}
             />
-            <label htmlFor={animals[0].label}>
-              {animals[0].label}
+            <label htmlFor={fields[0].label}>
+              {fields[0].label}
             </label>
 
             <input
-              id={animals[1].label}
+              id={fields[1].label}
               type="checkbox"
-              value={animals[1].label}
+              value={fields[1].label}
               onChange={onChange}
             />
-            <label htmlFor={animals[1].label}>
-              {animals[1].label}
+            <label htmlFor={fields[1].label}>
+              {fields[1].label}
             </label>
 
             <input
-              id={animals[2].label}
+              id={fields[2].label}
               type="checkbox"
-              value={animals[2].label}
+              value={fields[2].label}
               onChange={onChange}
             />
-            <label htmlFor={animals[2].label}>
-              {animals[2].label}
+            <label htmlFor={fields[2].label}>
+              {fields[2].label}
             </label>
+
             
-            <input
-              id={animals[3].label}
-              type="checkbox"
-              value={animals[3].label}
-              onChange={onChange}
-            />
-            <label htmlFor={animals[3].label}>
-              {animals[3].label}
-            </label>
 
             
             </div>
-      
-
-        {animals.map((animal) => {
-                return (
-                <div key={animal.label}>
-                    <input
-                    id={animal.label}
-                    type="checkbox"
-                    value={animal.label}
-                    onChange={onChange}
-                    />
-                    <label htmlFor={animal.label}>
-                    {animal.label}
-                    </label>
-                </div>
-                );
-            })}
             
     </div>
   );
