e60ea91fa749956954f74f7153a4c0c62eb1eb67
nachan28 <torajiro282828@gmail.com>
2023-04-02 16:17:39 +0900

    追加: スキルや分野をDBに登録する機能追加

--- my_app/src/pages/api/sendProfile.js
+++ my_app/src/pages/api/sendProfile.js
@@ -1,12 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import { use } from "react";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    console.log("kon")
-    const { username, introduction, background } = await req.body;
-    // console.log(user)
-    const update = await prisma.users.update({
+    const { username, introduction, background, user_s_skills, user_s_fields } = await req.body;
+    console.log("body: ", req.body)
+    const result = await prisma.users.findUnique({
+      where: {
+        username: username,
+      }
+    })
+    const updateUser = await prisma.users.update({
       where:{
         username: username
       },
@@ -14,9 +19,30 @@ export default async function handler(req, res) {
         introduction: introduction,
         background: background,
       },
-
     });
-    return res.json(update);
+
+    for (const skill_id of user_s_skills){
+      const record = await prisma.user_s_skills.create({
+        data: {
+          user_id: result.id,
+          skill_id: skill_id + 1
+        }
+      })
+    }
+
+    for (const field_id of user_s_fields){
+      const record = await prisma.user_s_fields.create({
+        data: {
+          user_id: result.id,
+          field_id: field_id + 1
+        }
+      })
+    }
+    
+
+    
+    // 何をreturnするかについては今後もうちょい検討
+    return res.json(updateUser);
   } else {
     res.status(405).json({ message: "Method Not Allowed" });
   }
